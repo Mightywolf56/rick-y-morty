dbcc0bd0625d2fbf76db68ad45747d2648b39a97
Jose Cienfuegos <114773971+Mightywolf56@users.noreply.github.com>
2023-02-08 15:47:34 -0700

    de nuevo actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Pagination from './components/Pagination'
 import ResidentList from './components/ResidentList'
 import ResidentForm from './components/ResidentForm'
 import Header from './components/Header'
-import Header from './components/Header'
+
 
 
 
